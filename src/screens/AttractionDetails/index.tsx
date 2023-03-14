0246e32e8c43bae78574ef2a2a29a53ba1be4e96
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2023-03-14 07:46:12 -0300

    class 41 add image gallery

--- src/screens/AttractionDetails/index.tsx
+++ src/screens/AttractionDetails/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import {ImageBackground, SafeAreaView, Text} from 'react-native';
+import {
+  Image,
+  ImageBackground,
+  Pressable,
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
 
 import styles from './styles';
 
@@ -16,11 +23,35 @@ function AttractionDetails({route, navigation}: any) {
       <ImageBackground
         style={styles.mainImage}
         imageStyle={{borderRadius: 20}}
-        source={{uri: mainImage}}
-      />
-      <Text onPress={onBack} style={{margin: 32}}>
-        Back
-      </Text>
+        source={{uri: mainImage}}>
+        <View style={styles.header}>
+          <Pressable onPress={onBack} hitSlop={8}>
+            <Image
+              style={styles.icon}
+              source={require('../../assets/back.png')}
+            />
+          </Pressable>
+          <Pressable hitSlop={8}>
+            <Image
+              style={styles.icon}
+              source={require('../../assets/share.png')}
+            />
+          </Pressable>
+        </View>
+
+        <View style={styles.footer}>
+          {item?.images?.length
+            ? item?.images?.map((image: any) => (
+                <Image
+                  key={image}
+                  source={{uri: image}}
+                  style={styles.miniImage}
+                />
+              ))
+            : null}
+        </View>
+      </ImageBackground>
+
       <Text>{item?.name}</Text>
     </SafeAreaView>
   );
--- src/screens/AttractionDetails/styles.ts
+++ src/screens/AttractionDetails/styles.ts
@@ -9,6 +9,35 @@ const styles = StyleSheet.create({
   mainImage: {
     width: '100%',
     height: height / 2,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 36,
+    height: 36,
+    margin: 16,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+    backgroundColor: 'rgba(256, 256, 256, 0.35)',
+    margin: 16,
+    paddingHorizontal: 8,
+  },
+  miniImage: {
+    width: 40,
+    height: 40,
+    margin: 8,
+    borderRadius: 10,
   },
 });
 
