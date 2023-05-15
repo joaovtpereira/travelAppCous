cec7f008c37376da09d854a449a3d801b14fa180
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2023-05-15 12:21:22 -0300

    add gallery

--- src/screens/Gallery/index.tsx
+++ src/screens/Gallery/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {Image, SafeAreaView, TouchableOpacity, View} from 'react-native';
 
 import styles from './styles';
+import {FlatList} from 'react-native-gesture-handler';
 
-function Gallery() {
+function Gallery({route, navigation}: any) {
+  const {images} = route.params || {};
+
+  const onBack = () => {
+    navigation.goBack();
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Galery</Text>
+      <View>
+        <FlatList
+          data={images}
+          style={{marginBottom: 24, paddingHorizontal: 24}}
+          renderItem={({item}) => (
+            <Image source={{uri: item}} style={styles.image} />
+          )}
+        />
+
+        <TouchableOpacity style={styles.backContainer} onPress={onBack}>
+          <Image
+            source={require('../../assets/back.png')}
+            style={styles.backIcon}
+          />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
--- src/screens/Gallery/styles.ts
+++ src/screens/Gallery/styles.ts
@@ -19,6 +19,21 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: 'rgba(0,0,0,0.5)',
   },
+  image: {
+    width: '100%',
+    height: 400,
+    borderRadius: 20,
+    marginBottom: 20,
+    marginTop: 24,
+  },
+  backContainer: {
+    position: 'absolute',
+    margin: 32,
+  },
+  backIcon: {
+    width: 40,
+    height: 40,
+  },
 });
 
 export default styles;
