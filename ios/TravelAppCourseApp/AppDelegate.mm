9c25b45d2ce4d78b26c3c75a64e9ee5a5d612541
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-01-06 14:48:42 -0300

    ajust build ios

--- ios/TravelAppCourseApp/AppDelegate.mm
+++ ios/TravelAppCourseApp/AppDelegate.mm
@@ -1,11 +1,14 @@
 #import "AppDelegate.h"
 
+#import <GoogleMaps/GoogleMaps.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"_YOUR_API_KEY_"]; // add this line using the api key obtained from Google Console
+  
   self.moduleName = @"TravelAppCourseApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
