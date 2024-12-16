2fac597d3e8a684bd1e0822688eb07d60dfdf566
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-12-16 08:32:02 -0300

    .

--- ios/TravelAppCourseApp/AppDelegate.mm
+++ ios/TravelAppCourseApp/AppDelegate.mm
@@ -7,7 +7,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"_YOUR_API_KEY_"]; // add this line using the api key obtained from Google Console
+  [GMSServices provideAPIKey:@"AIzaSyB9fSWrNdiK8FcKdpIowaDQGMEmGFBJHjI"]; // add this line using the api key obtained from Google Console
   
   self.moduleName = @"TravelAppCourseApp";
   // You can add your custom initial props in the dictionary below.
